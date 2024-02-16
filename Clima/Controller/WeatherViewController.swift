a45266c70f13805eabe114ee5747fc909fcc0844
bilal elsayed <bilalelsayed97@gmail.com>
2024-02-16 18:03:32 +0200

    1.UI Implemented 2.Created Models/Manger with MVC 3.Weather API Impelementaion

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,33 +1,47 @@
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var searchField: UITextField!
-    
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherMangerDelegate {
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var searchField: UITextField!
+
+    var weatherManger = WeatherManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        weatherManger.delegate = self
         searchField.delegate = self
-        
     }
-    
-    @IBAction func searchButtonPressed(_ sender: UIButton) {
+
+    @IBAction func searchButtonPressed(_: UIButton) {
         searchField.endEditing(true)
         print(searchField.text!)
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+    func textFieldShouldReturn(_: UITextField) -> Bool {
         searchField.endEditing(true)
         print(searchField.text!)
         return true
     }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.text = ""
-    }
-    
-}
 
+    func textFieldDidEndEditing(_: UITextField) {
+        if let city = searchField.text {
+            weatherManger.fetchWeather(cityName: city)
+        }
+
+//        textField.text = ""
+    }
+
+    func didUpdateWeather(_ weatherManger: WeatherManager,weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+        print(weather.temperature)
+    }
+
+    func didFailWithError(error: Error) {
+        print(" this a error testtttt \(error)")
+    }
+}
